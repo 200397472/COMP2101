38dc62810cf593bd014387e979c5a743dae350d9
200397472 <200397472@student.georgianc.on.ca>
2021-10-04 20:42:34 -0400

    LAB 22

---
+++ bash/hostname-changer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+hostnameofPC=$(hostname)
+echo "present hostname is $hostnameofPC"
+
+read -p "Insert your student number: " studentnumber
+
+string1="pc"
+
+finalstdnumber=$string1$studentnumber
+
+sed -i "s/$hostnameofPC/$finalstdnumber/" /etc/hosts
+
+if [[ $hostnameofPC != $finalstdnumber ]]
+then 
+	hostnamectl set-hostname $finalstdnumber
+fi
+	
+echo "Hostname is changed to $finalstdnumber from $hostnameofPC"
+
+echo "Restart your UBUNTU linux to get full effect of the script.."
